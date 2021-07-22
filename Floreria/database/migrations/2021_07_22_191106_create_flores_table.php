ae51e581617277b941541a9255d1bfaa3abfecda
DAVID CASTRO TRUJILLO <20183l301044@utcv.edu.mx>
2021-07-22 14:11:34 -0500

    Creacion de migraciones

---
+++ Floreria/database/migrations/2021_07_22_191106_create_flores_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFloresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flores', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flores');
+    }
+}
