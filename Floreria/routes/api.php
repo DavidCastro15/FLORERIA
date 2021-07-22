c37ed115de079b0cfc0821a2df0bccb333cc6dc5
DAVID CASTRO TRUJILLO <20183l301044@utcv.edu.mx>
2021-07-22 14:56:02 -0500

    ruta de pruebas

--- Floreria/routes/api.php
+++ Floreria/routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('flor',[FloresController::class, 'index']);
+
+//Route::get('flores',[FloresController::class, 'test']);
