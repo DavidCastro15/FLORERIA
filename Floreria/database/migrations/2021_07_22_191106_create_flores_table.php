3b53a8a3a2227f72b7fb5e5d74f4e770d0d1b5ea
DAVID CASTRO TRUJILLO <20183l301044@utcv.edu.mx>
2021-07-22 14:17:38 -0500

    Edicion de migraciones

--- Floreria/database/migrations/2021_07_22_191106_create_flores_table.php
+++ Floreria/database/migrations/2021_07_22_191106_create_flores_table.php
@@ -15,6 +15,10 @@ class CreateFloresTable extends Migration
     {
         Schema::create('flores', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('price');
+            $table->string('url_toImage');
             $table->timestamps();
         });
     }
