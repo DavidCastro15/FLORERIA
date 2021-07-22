9b1e265befe97ab5cd01e27816c86696f46e691a
DAVID CASTRO TRUJILLO <20183l301044@utcv.edu.mx>
2021-07-22 14:58:56 -0500

    Documentando modelo

--- Floreria/app/Models/Flor.php
+++ Floreria/app/Models/Flor.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flor extends Model
 {
+
+    //hacemos referncia a la tabla de la base de datos para poder usarla en el controlador
     protected $table = 'flores';
     use HasFactory;
 }
