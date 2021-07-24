5b46dfc899acf33f5019b5c15d69224d0511e5dc
Yadira PValerdi <pvyadira0@gmail.com>
2021-07-24 16:57:26 -0500

    Creación de la carpeta interfaces y del archivo info-pagina.interface.ts

---
+++ FrontEnd/src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,12 @@
+export interface InfoPagina 
+    {
+        titulo?: string;
+        email?: string;
+        nombre_corto?: string;
+        telefono?: string;
+        facebook?: string;
+        equipo_trabajo?: any [];
+    
+    }
+
+ 
--- FrontEnd/src/app/services/info-pagina.service.ts
+++ FrontEnd/src/app/services/info-pagina.service.ts
@@ -1,26 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  info: any = {};
+  info: InfoPagina = {};
   cargada = false;
 
-  constructor(private http:HttpClient) {
+  constructor( private http: HttpClient) {
 
-console.log('servicio de InfoPagina listo');
+//console.log('servicio de InfoPagina listo');
 //leer el archivo JSON 
 this.http.get('assets/data/data-pagina.json')
-.subscribe(resp => {
+.subscribe( (resp: InfoPagina) => {
 
   this.cargada = true;
   this.info = resp;
   
-  //console.log(resp);
-  console.log( resp['email'] );
+  console.log(resp);
+  //console.log( resp ['email'] );
 })
 
    }
